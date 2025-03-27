99114c2a383529c9b8f2b507109e962d129963ce
jamesredd64 <99228997+jamesredd64@users.noreply.github.com>
2025-03-27 15:00:26 -0400

    Remove development backup files from git tracking

--- _Dev/vite-frontend/fe-saved/src/components/YourComponent.tsx
+++
@@ -1,8 +0,0 @@
-import GroupIcon from '../icons/group.svg?react';
-
-// Use it like this:
-const YourComponent = () => {
-  return <GroupIcon className="w-6 h-6" />;
-};
-
-export default YourComponent;
