92db6839efcbf02b8beff73155374f3595cccde8
jamesredd64 <99228997+jamesredd64@users.noreply.github.com>
2025-03-30 02:52:25 -0400

    vercel and config to fix app not found

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  base: '/',
   plugins: [
     react(),
     svgr({
