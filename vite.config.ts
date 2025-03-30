aebdb7345d17cafea0638cc3ed0da7aef5c86862
jamesredd64 <99228997+jamesredd64@users.noreply.github.com>
2025-03-30 02:56:28 -0400

    vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig({
-  base: '/',
+export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     svgr({
