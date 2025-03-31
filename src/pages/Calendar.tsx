83ff3cf9c10e649ce0d43e77537f040de2532df5
jamesredd64 <99228997+jamesredd64@users.noreply.github.com>
2025-03-30 20:05:48 -0400

    reset

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -15,7 +15,7 @@ import Toast from "../components/ui/Toast";
 import { useLocation } from 'react-router-dom';
 import { useCalendar } from "../context/CalendarContext";
 import PageBreadcrumb from "../components/common/PageBreadCrumb";
-// import { getImageUrl } from '../config/images.config';
+
 
 interface CalendarEvent extends EventInput {
   extendedProps: {
