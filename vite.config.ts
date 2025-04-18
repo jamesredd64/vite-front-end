8e004392912a0889120f56b9233e38b648e88ef5
jamesredd64 <99228997+jamesredd64@users.noreply.github.com>
2025-04-18 02:49:11 -0400

    fix: react svg issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import react from '@vitejs/plugin-react';
-// import svgr from "vite-plugin-svgr";
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        // SVGR options here
+        icon: true,
+        svgo: true,
+      },
+    }),
+  ],
   build: {
     outDir: 'dist',
     sourcemap: true,
@@ -18,7 +26,6 @@ export default defineConfig({
       }
     }
   },
-  
   server: {
     port: 3000,
     cors: true,
