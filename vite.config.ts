40d5b874cbd8d45e234e9dd6e58552815f8cd18e
jamesredd64 <99228997+jamesredd64@users.noreply.github.com>
2025-04-18 02:41:35 -0400

    fix: yarn

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import react from '@vitejs/plugin-react';
 // import svgr from "vite-plugin-svgr";
 
