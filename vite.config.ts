7a8cbaa86b7ec6e339a4b45aa5e8055e89e3c274
jamesredd64 <99228997+jamesredd64@users.noreply.github.com>
2025-04-18 02:27:52 -0400

    fix: resolve build dependencies and remove package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      svgrOptions: {
-        icon: true,
-        exportType: "default",
-      },
-    }),
-  ],
+  
   build: {
     outDir: 'dist',
     sourcemap: true,
