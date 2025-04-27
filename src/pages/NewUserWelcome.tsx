b313f7b798af59310fb094e7bcd3410430e80284
jamesredd64 <99228997+jamesredd64@users.noreply.github.com>
2025-04-27 02:21:39 -0400

    fix: new welcome page link

--- src/pages/NewUserWelcome.tsx
+++ src/pages/NewUserWelcome.tsx
@@ -9,7 +9,7 @@ const NewUserWelcome: React.FC = () => {
   const { user } = useAuth0();
   
   const handleProfileClick = () => {
-    navigate('/profile-view');
+    navigate('/profile');
   };
 
   return (
